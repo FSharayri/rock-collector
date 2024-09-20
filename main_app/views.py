4ad05c5cb8b3199b6a7b22ba5f3ca0ec35cc15a2
Faris <farissharayri@gmail.com>
2024-09-20 07:37:35 -0400

    creates rock index DTL

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# Add the Cat class & list and view function below the imports
+class Rock:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, color, description, size):
+    self.name = name
+    self.color = color
+    self.description = description
+    self.size = size
+
+rocks = [
+    Rock('rocky','brown','was cast in a movie','big'),
+    Rock('stone','grey','always stoned','medium'),
+    Rock('pebble','white','annoying','small'),
+]
+
 def home(request):
     return render(request, 'base.html')
 
@@ -9,3 +23,6 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+def rocks_index(request):
+
+    return render(request,'rocks/index.html', {'rocks':rocks})
